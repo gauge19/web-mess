cdb22d33a7507f635c7a560981700727672eed9d
Georg <55636135+gauge19@users.noreply.github.com>
2019-10-16 12:11:00 +0200

    added "tests" folder
    used for js, html & css testing

---
+++ tests/main.js
@@ -0,0 +1,7 @@
+var input1 = document.getElementById("input1");
+var demo = document.getElementById("demo");
+demo.innerHTML = input1.value;
+
+function btn1_action() {
+  demo.innerHTML = input1.value;
+}
