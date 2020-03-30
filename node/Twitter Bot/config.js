2fe7b65cdaed93c88c09eb15714b7bf9238ab2b9
Georg <55636135+gauge19@users.noreply.github.com>
2020-03-30 20:34:58 +0200

    deleted node folder
    so as not to clutter the repo. might be moved to a new repo in the future.

--- node/Twitter Bot/config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    consumer_key: 'JWNBXS3PMPdQz4MT9mmHm4fk0',
-    consumer_secret: 'wRbfBesov5MiZ1mstXL9fZMmzb9BtDRJNnJPqGhnKwKFZTwWKi',
-    access_token: '2202206734-Jy7Y6SihKJeHK3dWeXLG3HZmGBe66Rut8n9hH7v',
-    access_token_secret: 'dTJdzapdCt5IUV2aYLgA7lNBe2LtjyvdCVFI8QTxgdkOv',
-    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL: true,     // optional - requires SSL certificates to be valid.
-};
--- node/bvg-departures/ressources/js/keys.js
+++
@@ -1,5 +0,0 @@
-// stores various API keys
-module.exports = {
-    nasa: "7cXSJzoHpkKdltAY2mVvqjxTJEJXk68xhEdQCh4h",
-    google: "AIzaSyDTUSe7oemC70SFPtSAj_VMafkr8JvT4Po"
-};
